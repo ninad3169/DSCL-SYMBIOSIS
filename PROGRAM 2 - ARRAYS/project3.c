78d7ec98b112fc5d4896a5e7bc31e30d24fdad6e
ninad3169 <77662182+ninad3169@users.noreply.github.com>
2023-04-25 22:43:34 -0700

    Create project3.c

---
+++ PROGRAM 2 - ARRAYS/project3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	int varA[15]={10,20,15,7,21};
+	int i;
+	printf("enter element of array:");
+	for(i=0;i<15;i++)
+	{
+		scanf("%d",&varA[15]);
+		prinf("%d",&varA[15]);
+	}
+	
+ 
+	
+	return 0;
+	
+}
+
